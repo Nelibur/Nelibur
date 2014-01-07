ea71299f1722360313580c43e29049196dc97721
GSerjo <sedot8@gmail.com>
2014-01-08 03:58:01 +0400

    *

--- Source/UnitTests/ForTest.cs
+++ Source/UnitTests/ForTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Xunit;
+﻿using Xunit;
 
 namespace UnitTests
 {
@@ -8,22 +6,6 @@ namespace UnitTests
     {
         [Fact]
         public void Test()
-        {
-            var container = new Container();
-            container.Register(() => new Item());
-        }
-
-        private sealed class Container
-        {
-            private readonly Dictionary<Type, Func<object>> _repository = new Dictionary<Type, Func<object>>();
-
-            public void Register<T>(Func<T> func)
-            {
-                _repository[typeof(T)] = () => func();
-            }
-        }
-
-        private sealed class Item
         {
         }
     }
