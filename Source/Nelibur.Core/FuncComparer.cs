57d18689791923c7c5f906138806b8939505ed66
GSerjo <sedot8@gmail.com>
2014-10-29 19:03:00 +0300

    + FuncComparer + FuncComparerTests

---
+++ Source/Nelibur.Core/FuncComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nelibur.Core
+{
+    public sealed class FuncComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> _comparer;
+
+        public FuncComparer(Func<T, T, bool> comparer)
+        {
+            if (comparer == null)
+            {
+                throw Error.ArgumentNull("comparer");
+            }
+            _comparer = comparer;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return _comparer(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj.ToString().ToLower().GetHashCode();
+        }
+    }
+}
