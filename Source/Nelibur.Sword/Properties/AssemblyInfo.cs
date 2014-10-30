8b77bb0379a129e59224a1174ff1e27509e04e41
Rykov.Sergey <Sergey.Rykov@infotecs.ru>
2014-10-30 17:09:22 +0300

    * Nelibur.Sword AssemblyInfo.cs assembly description to new name

--- Source/Nelibur.Sword/Properties/AssemblyInfo.cs
+++ Source/Nelibur.Sword/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Nelibur.Core")]
+[assembly: AssemblyTitle("Nelibur.Sword")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCulture("")]
 
