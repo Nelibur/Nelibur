e89b58ac926e53dcde90c8988889c1ab3598c940
SergioRykov <sergior@bk.ru>
2014-11-08 20:58:21 +0300

    + restore.cmd restores packages: both for project and tools

--- restore.cmd
+++ restore.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+rem Partially based on ideas from https://github.com/loresoft/KickStart
+
 md packages
 .nuget\nuget restore Solution.sln -PackagesDirectory packages
 .nuget\nuget install Tools\packages.config -OutputDirectory Tools -ExcludeVersion -NonInteractive
