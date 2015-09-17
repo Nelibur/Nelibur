c564b42fcf542e9d70839a1b3cb47c1688d61db8
GSerjo <sedot8@gmail.com>
2015-09-17 22:40:55 +0300

    + DictionaryExtensions

---
+++ Source/Nelibur.Sword/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Nelibur.Sword.DataStructures;
+
+namespace Nelibur.Sword.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static Option<TValue> GetValue<TKey, TValue>(this IDictionary<TKey, TValue> value, TKey key)
+        {
+            TValue result;
+            bool exists = value.TryGetValue(key, out result);
+            return new Option<TValue>(result, exists);
+        }
+    }
+}
---
+++ Source/UnitTests/Nelibur.Sword/Extensions/DictionaryExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Nelibur.Sword.DataStructures;
+using Nelibur.Sword.Extensions;
+using Xunit;
+
+namespace UnitTests.Nelibur.Sword.Extensions
+{
+    public sealed class DictionaryExtensionsTests
+    {
+        [Fact]
+        public void GetValue_Absent_Empty()
+        {
+            var dictiionary = new Dictionary<string, int> { { "A", 1 } };
+            Option<int> item = dictiionary.GetValue("B");
+
+            Assert.False(item.HasValue);
+        }
+
+        [Fact]
+        public void GetValue_Exists_Value()
+        {
+            var dictiionary = new Dictionary<string, int> { { "A", 1 } };
+            Option<int> item = dictiionary.GetValue("A");
+
+            Assert.True(item.HasValue);
+            Assert.Equal(1, item.Value);
+        }
+    }
+}
